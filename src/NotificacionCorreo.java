224620d843c42ac5bb5d9ff9590a19130dee6147
Luis Miguel <luis.hernandezbermeo@estudiantesunibague.edu.co>
2026-02-06 08:52:43 -0500

    implementacion notificacion por correo

---
+++ src/NotificacionCorreo.java
@@ -0,0 +1,11 @@
+public class NotificacionCorreo extends Notificacion {
+
+    protected NotificacionCorreo(String receptorNotificacion ) {
+        super ( receptorNotificacion);
+    }
+    @Override
+    public void enviarMensaje( String mensaje ) {
+        System.out.println("correo : " mensaje + " a : " + receptorNotificacion);
+
+    }
+}
