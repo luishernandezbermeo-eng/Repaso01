74249e74dd5e49aa423911220d4e3ad1288c87e5
Luis Miguel <luis.hernandezbermeo@estudiantesunibague.edu.co>
2026-02-06 09:11:23 -0500

    implementacion herencia y poliformismo

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        CuentaBancaria cuenta1 = new CuentaBancaria( id: "1", saldoInicial: 100);
+        cuenta1.retirar(valorARetirar: 100);
+        cuenta1.consignar ( valorAConsignar: 150);
+        System.out.println("Elsaldo de la cuenta es: " + cuenta1.getSaldo());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Notificacion [ ] notificacion = new Notificacion [ 2];
+        Notificacion [ 0] new = NotificacionCorreo ( receptorNotifiacion : "Pepito");
+        Notificacion [1] new NotificacionSMS (receptorNotificacion: "Juanillo");
+
+        notificacion[0 ].enviarMensaje( "hola mi amigo");
+        notificacion[1].enviarMensaje( "Hola mi broco");
     }
 }
